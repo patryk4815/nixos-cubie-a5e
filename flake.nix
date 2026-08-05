0b372c01650fdbb3ce13fa689db877d80fc95b0b
Patryk Sondej <patryk.sondej@gmail.com>
2026-08-05 21:39:19 +0200

    fix sunxi

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
     }) // {
       aarch64-linux = {
         sunxi-fel = nixpkgs-unstable.legacyPackages.aarch64-linux.sunxi-tools.overrideAttrs (old: {
-          meta = old.meta // { mainProgram = "sunxi-fel"; platforms = old.meta.platforms ++ [ system ]; };
+          meta = old.meta // { mainProgram = "sunxi-fel"; platforms = old.meta.platforms ++ [ "aarch64-linux" ]; };
         });
         uboot-vendor = uboot.vendor;
         uboot-1gb = uboot.mainline-1gb;
